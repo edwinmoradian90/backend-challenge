379c3c6780234549b6c0aedf1ec874c9a0767513
Edwin Moradian <edwin.moradian90@gmail.com>
2020-12-23 20:43:40 +0400

    adding dotenv

--- server/app.js
+++ server/app.js
@@ -45,9 +45,10 @@ app
       ],
     });
 
-    const fileName = `user-data-${user.id}.json`;
-    const filePath = path.join(__dirname, `/data/${fileName}`);
-    fs.writeFileSync(filePath, JSON.stringify(user));
+    fs.writeFileSync(
+      path.join(__dirname, `/data/user-data${user.id}.json`),
+      JSON.stringify(user)
+    );
 
     res.status(200).json(user);
   })
